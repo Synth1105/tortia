1308a3e88c129da08a219d20c51924649b080cdf
K <mango1472@outlook.kr>
2026-03-09 20:19:17 +0900

    fix echo

--- examples/custom/package-manager-plugins/noop.sh
+++ examples/custom/package-manager-plugins/noop.sh
@@ -3,15 +3,5 @@ set -eu
 
 action="${1:-}"
 
-case "$action" in
-  prepare)
-    # Nothing to prepare for this sample plugin.
-    ;;
-  auto-install)
-    # No dependencies for this sample.
-    ;;
-  *)
-    echo "unknown action: $action" >&2
-    exit 2
-    ;;
-esac
+# No-op: do nothing regardless of action.
+# The case statement is removed to prevent errors for unknown actions.
--- examples/custom/runtime-plugins/echo.sh
+++ examples/custom/runtime-plugins/echo.sh
@@ -1,17 +1,8 @@
 #!/bin/sh
 set -eu
 
-action="${1:-}"
-[ "$action" = "install" ]
+# Install echo-lang into the current directory.
 
-runtime_root="${TORTIA_RUNTIME_ROOT:?}"
-bin_file="${TORTIA_RUNTIME_BIN_PATHS_FILE:?}"
 
-mkdir -p "$runtime_root/bin"
-cat > "$runtime_root/bin/echo-lang" <<'SCRIPT'
-#!/bin/sh
 echo "hello from custom runtime plugin"
-SCRIPT
-chmod +x "$runtime_root/bin/echo-lang"
 
-echo "bin" > "$bin_file"
